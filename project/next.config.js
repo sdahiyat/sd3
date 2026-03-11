926509b84c84444a0c36971e3f52f61ce93d7886
Sierra Dahiyat <123037448+sdahiyat@users.noreply.github.com>
2026-03-11 16:44:04 -0700

    scaffold: add next.config.js

--- project/next.config.js
+++ project/next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['pdf-parse']
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**'
+      }
+    ]
+  },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
